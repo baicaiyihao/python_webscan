faae8dcce7ce5636afd71160f0cb6bc658fd60fc
baicaiyihao <stom698@163.com>
2021-01-08 16:20:48 +0800

    first commit

--- nmap_ip_scan.py
+++ nmap_ip_scan.py
@@ -1,51 +1,40 @@
+#-*- encoding=utf-8 -*-
 import nmap
-import threading
 import time
 
-lock = threading.Lock()
-opennum = 0
-threads = []
-time_start = time.time()
+list = []
+
+def main():
+    file_test = open('ip.txt')
+    with file_test as f:
+        text = f.read()
+    length = len(text.splitlines())  # 读取文件计算文件的行数长度
+    file_test.close()
+    file_test = open('ip.txt')
+    for i in range(0, length):  # 跟据文件的行数进行循环
+        file_line = file_test.readline()
+        remove_n = file_line.strip()  # 输出文件每行的内容
+        list.append(remove_n)  # 将文件每行的内容加入list列表
+        time.sleep(0.3)
+    file_test.close()
+main()
 
 def portscaner():
-    global opennum
-    lock.acquire()
-
-    try:
-        n = nmap.PortScanner()
-        n.scan(hosts="www.jju.edu.cn", arguments="-n -Pn -v -p 1-65535")        #配置nmap参数
-        for x in n.all_hosts():
-            host = x
-            for y in n[x].all_protocols():
-                for z in n[x][y].keys():
-                    port1 =  (host+':'+str(z))
-                    print(port1)
-                    ip_alive_port = open('ip_alive_port.txt', 'a')     #输出ip+端口至ip_alive_port.txt
-                    ip_alive_port.write(port1)
-                    ip_alive_port.write('\n')
-                    ip_alive_port.close()
-    except:pass
-
-    lock.release()
-
-# def main():
-#     setdefaulttimeout(0.1)
-#     time_start = time.time()
-#     for p in Port:
-#         t = threading.Thread(target=portscaner)
-#         threads.append(t)
-#         t.start()
-#     time_end = time.time()
-#     time_length = time_end - time_start
-#     for t in threads:
-#         t.join()
+    for host1 in list:
+        try:
+            n = nmap.PortScanner()
+            n.scan(hosts=host1, arguments="-n -Pn -v -p 80,443,445")        #配置nmap参数,-p参数后面添加端口
+            for x in n.all_hosts():
+                host = x
+                for y in n[x].all_protocols():
+                    for z in n[x][y].keys():
+                        port1 =  (host+':'+str(z))
+                        print(port1)
+                        ip_alive_port = open('ip_alive_port.txt', 'a')     #输出ip+端口至ip_alive_port.txt
+                        ip_alive_port.write(port1)
+                        ip_alive_port.write('\n')
+                        ip_alive_port.close()
+        except:pass
 
 if __name__ == '__main__':
-    t1 = threading.Thread(target=portscaner())
-    t1.start()
-    for t1 in threads:
-        t1.join()
-
-time_end = time.time()
-time_length = time_end - time_start
-print('运行时间：'+str(time_length))
+    portscaner()
